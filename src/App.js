87d9ea06daf1df2ec8c5def293964f3806de027f
sai6855 <60743144+sai6855@users.noreply.github.com>
2022-05-31 10:27:03 +0530

    added useTranistion hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState, useTransition } from "react";
 import "./styles.css";
 import Chart from "react-apexcharts";
 
@@ -11,106 +11,93 @@ const series = [
 
 export default function App() {
   const [activeScreen, setActiveScreen] = useState("bar");
-  const [isMounted, setIsMounted] = useState(false);
+  const [isPending, startTransition] = useTransition();
   const options = {
     chart: {
-      id: "basic-bar",
-      events: {
-        // beforeMount: function (chartContext, config) {
-        //   // ...
-        //   setIsMounted(true);
-        // }
-        mounted: function () {
-          setIsMounted(true);
-        }
-      }
+      id: "basic-bar"
     },
     xaxis: {
       categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
     }
   };
 
-  // useEffect(() => {
-  //   const a = () => console.log("loading");
-  //   window.addEventListener("load", a);
-
-  //   return () => window.removeEventListener("load", a);
-  // }, []);
-
-  console.log({ isMounted });
   return (
     <div className="App">
       <button
         onClick={() => {
-          setIsMounted(false);
-          setActiveScreen((state) => (state === "bar" ? "line" : "bar"));
-          console.log(
-            `showing screen ${activeScreen === "bar" ? "line" : "bar"}`
+          startTransition(() =>
+            setActiveScreen((state) => (state === "bar" ? "line" : "bar"))
           );
         }}
       >
         togglescreen
       </button>
 
-      {!isMounted && <p>loading...</p>}
-
-      {activeScreen === "bar" && (
-        <>
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-          <Chart options={options} series={series} type="bar" width="500" />
-        </>
-      )}
-      {activeScreen === "line" && (
-        <>
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-          <Chart options={options} series={series} type="line" width="500" />
-        </>
-      )}
+      {activeScreen === "bar" ? (
+        !isPending ? (
+          <>
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+            <Chart options={options} series={series} type="bar" width="500" />
+          </>
+        ) : (
+          <div>Loading....</div>
+        )
+      ) : null}
+      {activeScreen === "line" ? (
+        !isPending ? (
+          <>
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+            <Chart options={options} series={series} type="line" width="500" />
+          </>
+        ) : (
+          <div>Loading....</div>
+        )
+      ) : null}
     </div>
   );
 }
